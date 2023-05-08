73e940743f1635bff29e1f948a335e5163e1d54a
lan <59274072+lan-hx@users.noreply.github.com>
2023-05-09 00:27:47 +0800

    fix bug: FreeRTOS timer priority too low

--- Core/Inc/FreeRTOSConfig.h
+++ Core/Inc/FreeRTOSConfig.h
@@ -78,7 +78,7 @@ extern uint32_t SystemCoreClock;
 
 /* Software timer definitions. */
 #define configUSE_TIMERS 1
-#define configTIMER_TASK_PRIORITY (2)
+#define configTIMER_TASK_PRIORITY (55)
 #define configTIMER_QUEUE_LENGTH 10
 #define configTIMER_TASK_STACK_DEPTH 128
 
